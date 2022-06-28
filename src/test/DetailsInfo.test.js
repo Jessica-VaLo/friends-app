d77fa11fd66f1579053cd020576fb22141473f53
jessicava.1991 <jessicava.1991@gmail.com>
2022-06-28 17:19:27 -0400

    Testing DetailsInfo component

---
+++ src/test/DetailsInfo.test.js
@@ -0,0 +1,18 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import DetailsInfo from "./../component/DetailsInfo";
+
+describe("Friend Details Info", () => {
+  test("Checking renderization of its childrens", () => {
+    const infoMock = { bio: "I'm very friendly", phone: "53421" };
+
+    render(<DetailsInfo info={infoMock} />);
+    const el = screen.getByText(/Bio/i);
+    const el1 = screen.getByText(/friendly/i);
+    const el2 = screen.getByText(/53421/i);
+
+    expect(el).toBeInTheDocument();
+    expect(el1).toBeInTheDocument();
+    expect(el2).toBeInTheDocument();
+  });
+});
