a0004b778bd5725fac3fd681ae7c94fb9a1f4931
jessicava.1991 <jessicava.1991@gmail.com>
2022-06-27 22:38:25 -0400

    Creating and styling FriendProfile component where main data of friend is presented

--- src/component/FriendsAppContainer.jsx
+++ src/component/FriendsAppContainer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import FriendProfile from "./FriendProfile";
+import friend from "../friendDetails.json";
+import profileStyles from "../css/FriendProfileBg.module.css";
+import styles from "../css/FriendsAppContainer.module.css";
 
 function FriendsAppContainer() {
-  return <div>FriendsAppContainer</div>;
+  return (
+    <div className={styles.container}>
+      <FriendProfile friend={friend} propStyles={profileStyles} />
+    </div>
+  );
 }
 
 export default FriendsAppContainer;
