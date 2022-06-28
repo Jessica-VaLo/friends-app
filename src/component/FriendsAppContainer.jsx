59e5c3b5375105e13e16e2e5e884633cae30c925
jessicava.1991 <jessicava.1991@gmail.com>
2022-06-28 14:02:42 -0400

    Adding conditional rendering for showing FriendsList or FriendsDetails

--- src/component/FriendsAppContainer.jsx
+++ src/component/FriendsAppContainer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import friends from "../friendsData.json";
 import friend from "../friendDetails.json";
 import styles from "../css/FriendsAppContainer.module.css";
@@ -6,19 +6,29 @@ import FriendsListContainer from "./FriendsListContainer";
 import FriendsDetails from "./FriendsDetails";
 
 function FriendsAppContainer() {
+  const [showDetails, setShowDetails] = useState(false);
+
   const handleShowDetails = () => {
     console.log("Show details");
+    setShowDetails(true);
   };
+
   const handleGoBack = () => {
     console.log("Go back");
+    setShowDetails(false);
   };
+
   return (
     <div className={styles.container}>
-      {/* <FriendsListContainer
-        friends={friends}
-        onShowDetails={handleShowDetails}
-      /> */}
-      <FriendsDetails friend={friend} onGoBack={handleGoBack} />
+      {!showDetails && (
+        <FriendsListContainer
+          friends={friends}
+          onShowDetails={handleShowDetails}
+        />
+      )}
+      {showDetails && (
+        <FriendsDetails friend={friend} onGoBack={handleGoBack} />
+      )}
     </div>
   );
 }
